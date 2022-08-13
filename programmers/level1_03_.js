6bc2c633ab55d9016d5e8fe762e3bb5d82982cba
Everyruru <71490171+smilemet@users.noreply.github.com>
2022-08-13 11:01:14 +0900

    프로그래머스

---
+++ programmers/level1_03_.js
@@ -0,0 +1,33 @@
+const input = "abcdefghijklmn.p";
+
+function solution(new_id) {
+  var answer = "";
+  answer = new_id
+    .toLowerCase()
+    .replace(/[^\w\-\.]/g, "")
+    .replace(/[\.]{2,}/g, ".")
+    .replace(/^\./, "")
+    .replace(/\.$/, "");
+
+  if (!answer) answer = "a";
+  if (answer.length >= 16) answer = answer.substring(0, 15).replace(/\.$/, "");
+  if (answer.length <= 2) answer = answer.padEnd(3, answer[answer.length - 1]);
+
+  return answer;
+}
+
+console.log(solution(input));
+
+// 다른 풀이
+function solution(new_id) {
+  const answer = new_id
+    .toLowerCase() // 1
+    .replace(/[^\w-_.]/g, "") // 2
+    .replace(/\.+/g, ".") // 3
+    .replace(/^\.|\.$/g, "") // 4
+    .replace(/^$/, "a") // 5
+    .slice(0, 15)
+    .replace(/\.$/, ""); // 6
+  const len = answer.length;
+  return len > 2 ? answer : answer + answer.charAt(len - 1).repeat(3 - len);
+}
