3ecd08b011b604b79870a9d20a3d8ad65fb0ef52
Everyruru <71490171+smilemet@users.noreply.github.com>
2022-08-20 23:33:13 +0900

    프로그래머스

--- programmers/level1_018.js
+++ programmers/level1_018.js
@@ -1,11 +1,9 @@
 // Lv. 1 문자열을 정수로 바꾸기
 
-const a = 10;
+const a = "+1.25";
 
 function solution(s) {
-  var answer = 0;
-
-  return answer;
+  return parseInt(s);
 }
 
-console.log(solution(a));
+console.log(solution(a), typeof solution(a));
