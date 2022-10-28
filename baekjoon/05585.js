49e90aceffd7a1a0673a07c39cd807267065cc70
Everyruru <71490171+smilemet@users.noreply.github.com>
2022-10-29 04:48:50 +0900

    백준

---
+++ baekjoon/05585.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+// const input = fs.readFileSync("/dev/stdin").toString()
+const input = fs.readFileSync("input.txt").toString();
+
+const change = [500, 100, 50, 10, 5, 1];
+let answer = 0;
+
+let num = 1000 - input;
+
+for (let i = 0; i < change.length; i++) {
+  if (num === 0) return;
+  if (num >= change[i]) {
+    answer += Math.floor(num / change[i]);
+    num = num % change[i];
+  }
+}
+
+console.log(answer);
